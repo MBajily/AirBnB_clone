3fdb8dfbde65d297c74748d752c2708973d3871c
Mohammed Elgaily <mbajily@gmail.com>
2024-05-18 19:43:31 +0300

    Create Amenity Model test.

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,56 +1,56 @@
 #!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
+"""Defines unittests for models/amenity.py.
 
 Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
+    TestAmenity_instantiation
+    TestAmenity_save
+    TestAmenity_to_dict
 """
 import os
 import models
 import unittest
 from datetime import datetime
 # from time import sleep
-from models.base_model import BaseModel
+from models.amenity import Amenity
 
 
 class TestCustomModelInstantiation(unittest.TestCase):
     def test_no_args_instantiates(self):
-        self.assertEqual(BaseModel, type(BaseModel()))
+        self.assertEqual(Amenity, type(Amenity()))
 
     def test_new_instance_stored_in_objects(self):
-        self.assertIn(BaseModel(), models.storage.all().values())
+        self.assertIn(Amenity(), models.storage.all().values())
 
     def test_id_is_public_str(self):
-        self.assertEqual(str, type(BaseModel().id))
+        self.assertEqual(str, type(Amenity().id))
 
     def test_created_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(BaseModel().created_at))
+        self.assertEqual(datetime, type(Amenity().created_at))
 
     def test_updated_at_is_public_datetime(self):
-        self.assertEqual(datetime, type(BaseModel().updated_at))
+        self.assertEqual(datetime, type(Amenity().updated_at))
 
     def test_two_models_unique_ids(self):
-        cm1 = BaseModel()
-        cm2 = BaseModel()
+        cm1 = Amenity()
+        cm2 = Amenity()
         self.assertNotEqual(cm1.id, cm2.id)
 
     def test_two_models_different_created_at(self):
-        cm1 = BaseModel()
+        cm1 = Amenity()
         # sleep(0.05)
-        cm2 = BaseModel()
+        cm2 = Amenity()
         self.assertLess(cm1.created_at, cm2.created_at)
 
     def test_two_models_different_updated_at(self):
-        cm1 = BaseModel()
+        cm1 = Amenity()
         # sleep(0.05)
-        cm2 = BaseModel()
+        cm2 = Amenity()
         self.assertLess(cm1.updated_at, cm2.updated_at)
 
     def test_str_representation(self):
         dt = datetime.today()
         dt_repr = repr(dt)
-        cm = BaseModel()
+        cm = Amenity()
         cm.id = "123456"
         cm.created_at = cm.updated_at = dt
         cmstr = cm.__str__()
@@ -60,25 +60,25 @@ class TestCustomModelInstantiation(unittest.TestCase):
         self.assertIn("'updated_at': " + dt_repr, cmstr)
 
     def test_args_unused(self):
-        cm = BaseModel(None)
+        cm = Amenity(None)
         self.assertNotIn(None, cm.__dict__.values())
 
     def test_instantiation_with_kwargs(self):
         dt = datetime.today()
         dt_iso = dt.isoformat()
-        cm = BaseModel(id="345", created_at=dt_iso, updated_at=dt_iso)
+        cm = Amenity(id="345", created_at=dt_iso, updated_at=dt_iso)
         self.assertEqual(cm.id, "345")
         self.assertEqual(cm.created_at, dt)
         self.assertEqual(cm.updated_at, dt)
 
     def test_instantiation_with_None_kwargs(self):
         with self.assertRaises(TypeError):
-            BaseModel(id=None, created_at=None, updated_at=None)
+            Amenity(id=None, created_at=None, updated_at=None)
 
     def test_instantiation_with_args_and_kwargs(self):
         dt = datetime.today()
         dt_iso = dt.isoformat()
-        cm = BaseModel("12", id="345", created_at=dt_iso, updated_at=dt_iso)
+        cm = Amenity("12", id="345", created_at=dt_iso, updated_at=dt_iso)
         self.assertEqual(cm.id, "345")
         self.assertEqual(cm.created_at, dt)
         self.assertEqual(cm.updated_at, dt)
@@ -86,13 +86,13 @@ class TestCustomModelInstantiation(unittest.TestCase):
 
 class TestCustomModelSave(unittest.TestCase):
     def test_one_save(self):
-        cm = BaseModel()
+        cm = Amenity()
         first_updated_at = cm.updated_at
         cm.save()
         self.assertLess(first_updated_at, cm.updated_at)
 
     def test_two_saves(self):
-        cm = BaseModel()
+        cm = Amenity()
         first_updated_at = cm.updated_at
         cm.save()
         second_updated_at = cm.updated_at
@@ -101,12 +101,12 @@ class TestCustomModelSave(unittest.TestCase):
         self.assertLess(second_updated_at, cm.updated_at)
 
     def test_save_with_arg(self):
-        cm = BaseModel()
+        cm = Amenity()
         with self.assertRaises(TypeError):
             cm.save(None)
 
     def test_save_updates_file(self):
-        cm = BaseModel()
+        cm = Amenity()
         cm.save()
         cmid = "CustomModel." + cm.id
         with open("file.json", "r") as f:
@@ -115,18 +115,18 @@ class TestCustomModelSave(unittest.TestCase):
 
 class TestCustomModelToDict(unittest.TestCase):
     def test_to_dict_type(self):
-        cm = BaseModel()
+        cm = Amenity()
         self.assertTrue(dict, type(cm.to_dict()))
 
     def test_to_dict_contains_correct_keys(self):
-        cm = BaseModel()
+        cm = Amenity()
         self.assertIn("id", cm.to_dict())
         self.assertIn("created_at", cm.to_dict())
         self.assertIn("updated_at", cm.to_dict())
         self.assertIn("__class__", cm.to_dict())
 
     def test_to_dict_contains_added_attributes(self):
-        cm = BaseModel()
+        cm = Amenity()
         cm.name = "John Doe"
         cm.age = 25
         cm_dict = cm.to_dict()
